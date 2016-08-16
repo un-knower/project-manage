c7c7a6c00e759961571743f20a9ef90e73d6a0b2
taiyong <taiyong@youzan.com>
2016-08-17 00:29:03 +0800

    项目开发

---
+++ project-manage-domain/src/main/java/org/tsxuehu/pm/service/project/ProjectService.java
@@ -0,0 +1,34 @@
+package org.tsxuehu.pm.service.project;
+
+import org.tsxuehu.pm.domain.project.Project;
+
+import java.util.List;
+
+/**
+ * Created by tsxuehu on 16/8/17.
+ */
+public class ProjectService {
+
+    public Long createProject(String name,String description){
+        return null;
+    }
+    public void update(){
+
+    }
+
+    public List<Project> getAllProject(){
+        return null;
+    }
+
+    public Project getProject(Long id){
+        return null;
+    }
+
+    public String createBranch(String name){
+        return null;
+    }
+
+    public void deleteBranch(String branchId){
+
+    }
+}
